669d00beb495d80f930642cff257561dd26c22b5
Paul <paul@paulgoesdeep.com>
2021-02-09 20:05:54 +0300

    Make resulting xml look not too terrible with highlight.js

--- app/js/components/Export.tsx
+++ app/js/components/Export.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Activity } from '../activity';
+import { HighlightedCode } from './HighlightedCode';
 
 function Export(props: Props) {
   const TCXContents = props.activity.toTCX();
@@ -7,14 +8,19 @@ function Export(props: Props) {
   const file = new Blob([TCXContents], {type: 'application/xml'});
   const href = URL.createObjectURL(file);
 
-  return <div className="container">
-          <a href={href} download="processed.tcx">Download</a>
-          <div>Exporting!</div>
-          <div>
-            {TCXContents}
-          </div>
+  return  <div className="container">
+            <h2 className='title is-2 is-spaced'>Export adjusted tcx</h2>
 
-        </div>;
+            <div className='buttons'>
+              <a href={href} className="button is-primary" download="processed.tcx">Download</a>
+            </div>
+
+            <div>
+              <HighlightedCode language="xml">
+                {TCXContents}
+              </HighlightedCode>
+            </div>
+          </div>;
 }
 
 interface Props {
