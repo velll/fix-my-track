d4a0b9ba7ec5672342e48724321286efd5e66490
Paul <paul@paulgoesdeep.com>
2021-02-14 19:10:22 +0300

    Snap interaction: snaps to vertexes

--- app/js/map/interactions.ts
+++ app/js/map/interactions.ts
@@ -1,5 +1,5 @@
 import Feature from 'ol/Feature';
-import { Modify, Select, Interaction } from 'ol/interaction';
+import { Modify, Select, Interaction, Snap } from 'ol/interaction';
 import { toLonLat } from 'ol/proj';
 import { pointerMove } from 'ol/events/condition';
 import LineString from 'ol/geom/LineString';
@@ -12,15 +12,15 @@ const forbidden = () => (false);
 
 const interactions = (features: Collection<Feature>, {onSelected, onDeSelected, onMoved}: InteractionHandlers): Interaction[] => {
   const mainFeature = features.item(0);
-  const lineString = (mainFeature.getGeometry() as LineString);
+  const line = (mainFeature.getGeometry() as LineString);
 
-  const select = new Select({condition: pointerMove, style: highlightStyle});
-
+  const select = new Select({});
+  // const select = new Select({condition: pointerMove});
 
   select.on('select', e => {
     if (e.selected.length > 0) {
-      const selected = lineString.getClosestPoint(e.mapBrowserEvent.coordinate);
-      const selectedIndex = lineString.getCoordinates().findIndex(c  => isSame(c, selected));
+      const selected = line.getClosestPoint(e.mapBrowserEvent.coordinate);
+      const selectedIndex = line.getCoordinates().findIndex(c  => isSame(c, selected));
 
       if (selectedIndex > -1) { onSelected(selectedIndex); }
     }
@@ -34,13 +34,19 @@ const interactions = (features: Collection<Feature>, {onSelected, onDeSelected, 
 
   modify.on('modifyend', e => {
     const feature = e.features.getArray()[0];
-    const lineString = feature.getGeometry() as LineString;
-    const newCoordinates = lineString.getCoordinates().map(coordinatePair => toLonLat(coordinatePair));
+
+    if (feature == null) { return; }
+
+    const geometry = feature.getGeometry() as LineString;
+    const newCoordinates = geometry.getCoordinates().map(coordinatePair => toLonLat(coordinatePair));
 
     onMoved(newCoordinates);
   });
 
-  return [select, modify];
+  const snap = new Snap({features: features, edge: false});
+
+  // Snap should be LAST as it affects all the interactions that follow
+  return [select, modify, snap];
 };
 
 type InteractionHandler = (arg0: number) => void;
