a628e49caa929abe96c1cfedc9ba056088f96638
Paul <paul@paulgoesdeep.com>
2021-03-24 22:01:57 +0300

    Log some errors

--- app/controllers/strava_controller.go
+++ app/controllers/strava_controller.go
@@ -3,12 +3,15 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/velll/fix-my-track/app/services/strava"
+	"log"
 )
 
 func ExchangeStravaToken(c *gin.Context) {
 	codes, exists := c.Request.URL.Query()["code"]
 
 	if !exists {
+		log.Println("Cannot authorize in strava: empty code received")
+
 		Index(c, "Cannot authorize in strava. Code should not be empty")
 		return
 	}
@@ -17,6 +20,8 @@ func ExchangeStravaToken(c *gin.Context) {
 	token, err := strava.Exchange(code)
 
 	if err != nil {
+		log.Println("Cannot authorize in strava. ", err)
+
 		Index(c, "Cannot authorize in strava")
 		return
 	}
