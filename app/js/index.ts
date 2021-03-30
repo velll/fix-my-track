d92c20a436f837b0c0d288842d35894c6cfd19cd
Paul <paul@paulgoesdeep.com>
2021-03-30 19:47:41 +0300

    Clean up entrypoint

--- app/js/index.ts
+++ app/js/index.ts
@@ -3,11 +3,5 @@ import * as React from "react";
 
 import Root from "./components/Root";
 
-
-console.log('hello');
-
 const e = React.createElement;
 const app = ReactDOM.render(e(Root, {}), document.getElementById("starter"));
-
-(window as any).app = app;
-// (window as any).map = map;
