a8b75b8453a2ee568389f0315274cf693d270005
Paul <paul@paulgoesdeep.com>
2021-04-03 17:07:32 +0300

    Slurp empty lines when rendering new tcx file

--- app/js/lib/tcx/encode.ts
+++ app/js/lib/tcx/encode.ts
@@ -2,11 +2,16 @@ import compileTemplate from 'lodash.template';
 import template from '!!raw-loader!./tcx.xml.erb';
 import { Activity } from '../../models/activity';
 import { prepare } from './prepare';
+import { cleanupEmptyLines } from '../cleanup-empty-lines';
 
 const compiled = compileTemplate(template);
 
 function encode(activity: Activity) {
-  return compiled({activity: prepare(activity)});
+  return  cleanupEmptyLines(
+            compiled(
+              { activity: prepare(activity) }
+            )
+          );
 }
 
 export { encode };
